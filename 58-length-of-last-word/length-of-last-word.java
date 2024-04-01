893dbcda542e3f44536e44afa2aa6b9deb232b72
Furktulia <138386138+Furktulia@users.noreply.github.com>
2024-04-01 15:52:24 +0200

    Time: 0 ms (100.00%) | Memory: 41.4 MB (86.56%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,6 +1,11 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] arr = s.split(" ");
-        return arr[arr.length - 1].length();
+        int ans = 0;
+        for(int i = s.length()-1; i>=0; --i){
+            if(s.charAt(i) == ' ' && ans == 0)continue;
+            if(s.charAt(i) == ' ') return ans;
+            ++ans;
+        }
+        return ans;
     }
 }
